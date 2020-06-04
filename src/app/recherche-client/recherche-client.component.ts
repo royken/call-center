755c5798619b6cc944d4d6aec887b1603ce758bd
Valmy-roi Kenfack <valmy_roi@yahoo.fr>
2020-06-04 08:45:03 +0100

    SurveyJs dependency added

--- src/app/recherche-client/recherche-client.component.ts
+++ src/app/recherche-client/recherche-client.component.ts
@@ -1,36 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import Swal from "sweetalert2";
 import {
   FormControl,
   FormBuilder,
   FormGroup,
-  Validators
+  Validators,
 } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import * as SurveyCreator from "survey-creator";
 
 @Component({
-  selector: 'app-recherche-client',
-  templateUrl: './recherche-client.component.html',
-  styleUrls: ['./recherche-client.component.scss']
+  selector: "app-recherche-client",
+  templateUrl: "./recherche-client.component.html",
+  styleUrls: ["./recherche-client.component.scss"],
 })
 export class RechercheClientComponent implements OnInit {
-
   numeroDeCompte: string;
   nomClient: string;
   clientsLoaded: Boolean = false;
   rechercheForm: FormGroup;
 
-  constructor(private route: ActivatedRoute,
+  options = {
+    showLogicTab: true,
+  };
+
+  constructor(
+    private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder,) { }
+    private formBuilder: FormBuilder
+  ) {
+    var creator = new SurveyCreator.SurveyCreator("creatorElement", this.options);
+    //Show toolbox in the right container. It is shown on the left by default
+    creator.showToolbox = "right";
+    //Show property grid in the right container, combined with toolbox
+    creator.showPropertyGrid = "right";
+    //Make toolbox active by default
+    creator.rightContainerActiveItem("toolbox");
+  }
 
   ngOnInit(): void {
-
     this.rechercheForm = this.formBuilder.group({
-      numeroDeCompte: ['', [Validators.maxLength(10), Validators.minLength(5)]],
-      nomClient: ['', [Validators.maxLength(20), Validators.minLength(3)]]
+      numeroDeCompte: ["", [Validators.maxLength(10), Validators.minLength(5)]],
+      nomClient: ["", [Validators.maxLength(20), Validators.minLength(3)]],
     });
-
   }
 
   initialize() {
@@ -38,8 +50,8 @@ export class RechercheClientComponent implements OnInit {
     this.nomClient = null;
   }
 
-  rechercheByNumero(){
-    console.log("Numero",this.rechercheForm.value.numeroDeCompte);
+  rechercheByNumero() {
+    console.log("Numero", this.rechercheForm.value.numeroDeCompte);
     /*Swal.fire({
       title: 'Error!',
       text: 'L\'organe est obligatoire',
@@ -48,8 +60,8 @@ export class RechercheClientComponent implements OnInit {
     })*/
   }
 
-  rechercheByNom(){
-    console.log("Nom",this.nomClient);
+  rechercheByNom() {
+    console.log("Nom", this.nomClient);
     /*Swal.fire(
       'Enregistrement!',
       'Sous Organe ' + data.nom + ' enregistré!',
@@ -57,9 +69,8 @@ export class RechercheClientComponent implements OnInit {
     )*/
   }
 
-  rechercheByNomAndNumero(){
-    console.log("Deux",this.numeroDeCompte);
-    this.router.navigate(['detail-client']);
+  rechercheByNomAndNumero() {
+    console.log("Deux", this.numeroDeCompte);
+    this.router.navigate(["detail-client"]);
   }
-
 }
